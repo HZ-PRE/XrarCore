54a3be663c55e0cad6b5cbefa2fc574afd7b60bc
林间有风 <1906171836@qq.com>
2025-12-31 15:06:33 +0800

    ss协议优化

--- proxy/shadowsocks/validator.go
+++ proxy/shadowsocks/validator.go
@@ -1,10 +1,12 @@
 package shadowsocks
 
 import (
+	"context"
 	"crypto/cipher"
 	"crypto/hmac"
 	"crypto/sha256"
 	"hash/crc64"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -17,15 +19,41 @@ import (
 // Validator stores valid Shadowsocks users.
 type Validator struct {
 	sync.RWMutex
-	users         sync.Map
-	legacyUsers   sync.Map
-	userSize      uint64
-	onUsers       sync.Map
-	onDayUsers    sync.Map
-	onHourUsers   sync.Map
-	behaviorSeed  uint64
-	behaviorFused bool
+	users          sync.Map
+	userSize       int
+	legacyUsers    sync.Map
+	onUsers        sync.Map
+	onUserSize     int
+	onDayUsers     sync.Map
+	onDayUserSize  int
+	onHourUsers    sync.Map
+	onHourUserSize int
+	behaviorSeed   uint64
+	behaviorFused  bool
 }
+type batchResult struct {
+	u     *protocol.MemoryUser
+	aead  cipher.AEAD
+	ret   []byte
+	ivLen int32
+	err   error
+}
+
+var (
+	subkeyPool = sync.Pool{
+		New: func() any {
+			b := make([]byte, 32)
+			return &b
+		},
+	}
+
+	dataPool = sync.Pool{
+		New: func() any {
+			b := make([]byte, 8192) // 用于 UDP 的较大缓冲区
+			return &b
+		},
+	}
+)
 
 var ErrNotFound = errors.New("Not Found")
 
@@ -37,7 +65,7 @@ func (v *Validator) Add(u *protocol.MemoryUser) error {
 	account := u.Account.(*MemoryAccount)
 	email := strings.ToLower(u.Email)
 	v.userSize = v.userSize + 1
-	if _, ok := v.legacyUsers.Load(email); ok && !account.Cipher.IsAEAD() {
+	if !account.Cipher.IsAEAD() {
 		v.legacyUsers.Store(email, u)
 		return nil
 	}
@@ -56,11 +84,13 @@ func (v *Validator) Del(email string) error {
 	if email == "" {
 		return errors.New("Email must not be empty.")
 	}
-
 	v.Lock()
 	defer v.Unlock()
 
 	email = strings.ToLower(email)
+	if _, ok := v.users.Load(email); !ok {
+		return nil
+	}
 	v.onUsers.Delete(email)
 	v.onHourUsers.Delete(email)
 	v.onDayUsers.Delete(email)
@@ -75,9 +105,6 @@ func (v *Validator) GetByEmail(email string) *protocol.MemoryUser {
 	if email == "" {
 		return nil
 	}
-
-	v.Lock()
-	defer v.Unlock()
 	email = strings.ToLower(email)
 	if value, ok := v.legacyUsers.Load(email); ok {
 		return value.(*protocol.MemoryUser)
@@ -90,9 +117,7 @@ func (v *Validator) GetByEmail(email string) *protocol.MemoryUser {
 
 // GetAll get all users
 func (v *Validator) GetAll() []*protocol.MemoryUser {
-	v.Lock()
-	defer v.Unlock()
-	var u = make([]*protocol.MemoryUser, 0, 100)
+	var u = make([]*protocol.MemoryUser, 0, 2000)
 	v.users.Range(func(key, value interface{}) bool {
 		u = append(u, value.(*protocol.MemoryUser))
 		return true
@@ -101,7 +126,7 @@ func (v *Validator) GetAll() []*protocol.MemoryUser {
 		u = append(u, value.(*protocol.MemoryUser))
 		return true
 	})
-	v.userSize = uint64(len(u))
+	v.userSize = len(u)
 	return u
 }
 
@@ -110,36 +135,44 @@ func (v *Validator) DetOnUsers() {
 	v.Lock()
 	defer v.Unlock()
 	newDate := time.Now()
-	v.onUsers.Range(func(key, value interface{}) bool {
-		m := value.(time.Time)
-		duration := newDate.Sub(m)
-		if duration > 10*time.Minute {
-			v.onUsers.Delete(key)
-		}
-		return true
-	})
-	v.onHourUsers.Range(func(key, value interface{}) bool {
-		m := value.(time.Time)
-		duration := newDate.Sub(m)
-		if duration > 6*time.Hour {
-			v.onHourUsers.Delete(key)
-		}
-		return true
-	})
+	onUserSize := 0
+	onHourUserSize := 0
+	onDayUserSize := 0
 	v.onDayUsers.Range(func(key, value interface{}) bool {
 		m := value.(time.Time)
 		duration := newDate.Sub(m)
 		if duration > 24*time.Hour {
 			v.onDayUsers.Delete(key)
+		} else {
+			onDayUserSize += 1
+			if v1, ok := v.onHourUsers.Load(key); ok {
+				m1 := v1.(time.Time)
+				duration = newDate.Sub(m1)
+				if duration > 6*time.Hour {
+					v.onHourUsers.Delete(key)
+				} else {
+					onHourUserSize += 1
+					if v2, ok2 := v.onUsers.Load(key); ok2 {
+						m2 := v2.(time.Time)
+						duration = newDate.Sub(m2)
+						if duration > 11*time.Minute {
+							v.onUsers.Delete(key)
+						} else {
+							onUserSize += 1
+						}
+					}
+				}
+			}
 		}
 		return true
 	})
+	v.onUserSize = onUserSize
+	v.onHourUserSize = onHourUserSize
+	v.onDayUserSize = onDayUserSize
 }
 
 // GetCount get users count
 func (v *Validator) GetCount() int64 {
-	v.Lock()
-	defer v.Unlock()
 	return int64(v.userSize)
 }
 
@@ -157,71 +190,177 @@ func (v *Validator) Get(bs []byte, command protocol.RequestCommand) (u *protocol
 		})
 		return
 	}
-	newDate := time.Now()
-	v.onUsers.Range(func(key, value interface{}) bool {
-		if user, ok := v.users.Load(key); ok {
-			u1 := user.(*protocol.MemoryUser)
-			u, aead, ret, ivLen, err = checkAEADAndMatch(bs, u1, command)
-			if u == nil {
-				return true
+	if v.onUserSize < 3000 {
+		v.onUsers.Range(func(key, value interface{}) bool {
+			if user, ok := v.users.Load(key); ok {
+				u1 := user.(*protocol.MemoryUser)
+				u, aead, ret, ivLen, err = checkAEADAndMatch(bs, u1, command)
+				if u == nil {
+					return true
+				}
+				return false
 			}
-			v.onUsers.Store(u.Email, newDate)
-			return false
-		}
-		return true
-
-	})
-	if u != nil {
-		return
-	}
-	v.onHourUsers.Range(func(key, value interface{}) bool {
-		if user, ok := v.users.Load(key); ok {
-			u1 := user.(*protocol.MemoryUser)
-			u, aead, ret, ivLen, err = checkAEADAndMatch(bs, u1, command)
-			if u == nil {
-				return true
-			}
-			v.onUsers.Store(u.Email, newDate)
-			v.onHourUsers.Store(u.Email, newDate)
-			return false
-		}
-		return true
-	})
-	if u != nil {
-		return
-	}
-	v.onDayUsers.Range(func(key, value interface{}) bool {
-		if user, ok := v.users.Load(key); ok {
-			u1 := user.(*protocol.MemoryUser)
-			u, aead, ret, ivLen, err = checkAEADAndMatch(bs, u1, command)
-			if u == nil {
-				return true
-			}
-			v.onUsers.Store(u.Email, newDate)
-			v.onHourUsers.Store(u.Email, newDate)
-			v.onDayUsers.Store(u.Email, newDate)
-			return false
-		}
-		return true
-	})
-	if u != nil {
-		return
-	}
-	v.users.Range(func(key, value interface{}) bool {
-		user := value.(*protocol.MemoryUser)
-		u, aead, ret, ivLen, err = checkAEADAndMatch(bs, user, command)
-		if u == nil {
 			return true
-		}
-		v.onUsers.Store(u.Email, newDate)
-		v.onHourUsers.Store(u.Email, newDate)
-		v.onDayUsers.Store(u.Email, newDate)
-		return false
-	})
+
+		})
+	} else {
+		u, aead, ret, ivLen, err = processUsersInBatchesParallel(nil, &v.users, &v.onUsers, bs, command, 3000)
+	}
 	if u != nil {
+		v.touchUser(u.Email)
+		return
+	}
+	if v.onHourUserSize < 5000 {
+		v.onHourUsers.Range(func(key, value interface{}) bool {
+			if _, ok := v.onUsers.Load(key); ok {
+				return true
+			}
+			if user, ok := v.users.Load(key); ok {
+				u1 := user.(*protocol.MemoryUser)
+				u, aead, ret, ivLen, err = checkAEADAndMatch(bs, u1, command)
+				if u == nil {
+					return true
+				}
+				return false
+			}
+			return true
+
+		})
+	} else {
+		u, aead, ret, ivLen, err = processUsersInBatchesParallel(&v.onUsers, &v.users, &v.onHourUsers, bs, command, 5000)
+	}
+	if u != nil {
+		v.touchUser(u.Email)
+		return
+	}
+	if v.onDayUserSize < 7000 {
+		v.onDayUsers.Range(func(key, value interface{}) bool {
+			if _, ok := v.onHourUsers.Load(key); ok {
+				return true
+			}
+			if user, ok := v.users.Load(key); ok {
+				u1 := user.(*protocol.MemoryUser)
+				u, aead, ret, ivLen, err = checkAEADAndMatch(bs, u1, command)
+				if u == nil {
+					return true
+				}
+				return false
+			}
+			return true
+
+		})
+	} else {
+		u, aead, ret, ivLen, err = processUsersInBatchesParallel(&v.onHourUsers, &v.users, &v.onDayUsers, bs, command, 7000)
+	}
+	if u != nil {
+		v.touchUser(u.Email)
+		return
+	}
+	u, aead, ret, ivLen, err = processUsersInBatchesParallel(&v.onDayUsers, nil, &v.users, bs, command, 14000)
+	if u != nil {
+		v.touchUser(u.Email)
 		return
 	}
 	return nil, nil, nil, 0, ErrNotFound
+}
+
+// 使用并行处理的批量函数
+func processUsersInBatchesParallel(topUsers *sync.Map, userList *sync.Map, users *sync.Map, bs []byte, command protocol.RequestCommand, batchSize int) (u *protocol.MemoryUser, aead cipher.AEAD, ret []byte, ivLen int32, err error) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	var (
+		wg     sync.WaitGroup
+		once   sync.Once
+		result = make(chan *batchResult, 1)
+		sem    = make(chan struct{}, runtime.GOMAXPROCS(0))
+	)
+
+	launch := func(batch []*protocol.MemoryUser) {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(b []*protocol.MemoryUser) {
+			defer wg.Done()
+			defer func() { <-sem }()
+			userProcessBatch(ctx, b, bs, command, cancel, result, &once)
+		}(batch)
+	}
+
+	batch := make([]*protocol.MemoryUser, 0, batchSize)
+
+	users.Range(func(key, value any) bool {
+		if topUsers != nil {
+			if _, ok := topUsers.Load(key); ok {
+				return true
+			}
+		}
+
+		var user *protocol.MemoryUser
+		if userList != nil {
+			v, ok := userList.Load(key)
+			if !ok {
+				return true
+			}
+			user = v.(*protocol.MemoryUser)
+		} else {
+			user = value.(*protocol.MemoryUser)
+		}
+
+		batch = append(batch, user)
+
+		if len(batch) == batchSize {
+			local := append([]*protocol.MemoryUser(nil), batch...)
+			batch = batch[:0]
+			launch(local)
+		}
+
+		select {
+		case <-ctx.Done():
+			return false
+		default:
+			return true
+		}
+	})
+
+	if len(batch) > 0 {
+		launch(append([]*protocol.MemoryUser(nil), batch...))
+	}
+
+	go func() {
+		wg.Wait()
+		cancel()
+	}()
+
+	select {
+	case r := <-result:
+		return r.u, r.aead, r.ret, r.ivLen, r.err
+	case <-ctx.Done():
+		return nil, nil, nil, 0, ErrNotFound
+	}
+}
+
+func userProcessBatch(ctx context.Context, batch []*protocol.MemoryUser, bs []byte, command protocol.RequestCommand, cancel context.CancelFunc, result chan<- *batchResult, once *sync.Once) {
+	for _, user := range batch {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			u, aead, ret, ivLen, err := checkAEADAndMatch(bs, user, command)
+			if u != nil {
+				once.Do(func() {
+					result <- &batchResult{
+						u:     u,
+						aead:  aead,
+						ret:   ret,
+						ivLen: ivLen,
+						err:   err,
+					}
+					cancel()
+				})
+				return
+			}
+		}
+	}
 }
 
 func (v *Validator) GetBehaviorSeed() uint64 {
@@ -234,26 +373,40 @@ func (v *Validator) GetBehaviorSeed() uint64 {
 	}
 	return v.behaviorSeed
 }
+func (v *Validator) touchUser(email string) {
+	now := time.Now()
+	v.onUsers.Store(email, now)
+	v.onHourUsers.Store(email, now)
+	v.onDayUsers.Store(email, now)
+}
 
 func checkAEADAndMatch(bs []byte, user *protocol.MemoryUser, command protocol.RequestCommand) (u *protocol.MemoryUser, aead cipher.AEAD, ret []byte, ivLen int32, err error) {
 	account := user.Account.(*MemoryAccount)
 	aeadCipher := account.Cipher.(*AEADCipher)
 	ivLen = aeadCipher.IVSize()
+	if ivLen < 8 {
+		return
+	}
 	iv := bs[:ivLen]
-	subkey := make([]byte, 32)
+	ptr := subkeyPool.Get().(*[]byte)
+	defer subkeyPool.Put(ptr)
+	subkey := *ptr
 	subkey = subkey[:aeadCipher.KeyBytes]
 	hkdfSHA1(account.Key, iv, subkey)
 	aead = aeadCipher.AEADAuthCreator(subkey)
 	var matchErr error
+	var dataPtr = dataPool.Get().(*[]byte)
+	defer dataPool.Put(dataPtr)
 	switch command {
 	case protocol.RequestCommandTCP:
-		data := make([]byte, 4+aead.NonceSize())
+		data := (*dataPtr)[:4+aead.NonceSize()]
 		ret, matchErr = aead.Open(data[:0], data[4:], bs[ivLen:ivLen+18], nil)
 	case protocol.RequestCommandUDP:
-		data := make([]byte, 8192)
+		data := (*dataPtr)[:8192]
 		ret, matchErr = aead.Open(data[:0], data[8192-aead.NonceSize():8192], bs[ivLen:], nil)
 	}
-
+	subkeyPool.Put(ptr)
+	dataPool.Put(dataPtr)
 	if matchErr == nil {
 		u = user
 		err = account.CheckIV(iv)
